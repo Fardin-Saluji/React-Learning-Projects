4f311e7986561eba910efa62c0f3ce5d692e41b4
fardin saluji <162831344+Fardin-Saluji@users.noreply.github.com>
2026-01-06 21:18:59 +0530

    Create App component with dynamic login form

---
+++ 08-dynamic-login-form/src/App.js
@@ -0,0 +1,12 @@
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  return (
+     <div style={{ textAlign: "center" }}>
+      <h1>Dynamic Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
