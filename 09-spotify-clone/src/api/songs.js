9886748967482610d1fd96c96774eb625e8c3b28
fardin saluji <162831344+Fardin-Saluji@users.noreply.github.com>
2026-01-13 20:32:53 +0530

    Create songs.js

---
+++ 09-spotify-clone/src/api/songs.js
@@ -0,0 +1,16 @@
+export const songs = [
+  {
+    id: 1,
+    title: "Night Vibes",
+    artist: "DJ Relax",
+    cover: "/cover/cover1.jpg",
+    src: "/songs/song1.mp3"
+  },
+  {
+    id: 2,
+    title: "Chill Beats",
+    artist: "LoFi King",
+    cover: "/cover/cover2.jpg",
+    src: "/songs/song2.mp3"
+  }
+];
