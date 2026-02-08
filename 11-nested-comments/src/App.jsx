4e7b7a8eb863fe5245713bff705ae9bd0e549b53
fardin saluji <162831344+Fardin-Saluji@users.noreply.github.com>
2026-02-08 10:17:06 +0530

    Create App.jsx

---
+++ 11-nested-comments/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommentSection from './components/CommentSection';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CommentSection />
+    </div>
+  );
+}
+
+export default App;
